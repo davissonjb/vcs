23b828d6bf11d7a31b31b0d6f787e2de7a369f0c
Jacob B. Davisson <davissonjb@gmail.com>
2024-06-08 15:50:55 -0500

    Working to make the recursive directory search more idiomatic Rust.

--- io/src/seek.rs
+++ io/src/seek.rs
@@ -61,22 +61,23 @@ impl Crawler {
 
     pub fn seek(&mut self) -> () {
         println!("Recursively seeking in root: {}", self.root.display());
-        for e in fs::read_dir(self.root.deref()).unwrap() {
-            let ent = e.unwrap();
-            println!("Entry:: {}\n", ent.path().display());
-            let p = ent.path();
-            println!("----entry.path :: {}", p.display());
-            let md = fs::metadata(&p).unwrap();
-            if md.is_dir() && ent.path().as_os_str() != "target" {
-                self.dirs.push(Box::new(p.clone()));
-                self.setnext(p.clone());
-                self.seekNext(*self.next.clone());
-            }
-            if md.is_file() {
-                self.files
-                    .push(Box::new(p.clone().file_name().unwrap().into()))
-            }
-        }
+        // for e in fs::read_dir(self.root.deref()).unwrap() {
+        //     let ent = e.unwrap();
+        //     println!("Entry:: {}\n", ent.path().display());
+        //     let p = ent.path();
+        //     println!("----entry.path :: {}", p.display());
+
+        // let md = fs::metadata(&p).unwrap();
+        // if md.is_dir() && ent.path().as_os_str() != "target" {
+        //     self.dirs.push(Box::new(p.clone()));
+        //     self.setnext(p.clone());
+        //     self.seekNext(*self.next.clone());
+        // }
+        // if md.is_file() {
+        //     self.files
+        //         .push(Box::new(p.clone().file_name().unwrap().into()))
+        // }
+        // }
     } // END pub fn seek(&mut self) -> ()
 
     fn seekNext(&mut self, p: PathBuf) -> () {
@@ -86,12 +87,10 @@ impl Crawler {
             let ent = e.unwrap();
             // println!("SeekNext - Entry:: {}\n", ent.path().display());
             let p = ent.path();
-            if (ent.file_name() == p.file_name().unwrap()) {
-                println!("They're the same!");
-            }
             // println!("----entry.path :: {}", p.display());
             let md = fs::metadata(&p).unwrap();
-            if md.is_dir() && (ent.path().as_os_str() != "target") {
+            if md.is_dir() {
+                //&& (ent.path().as_os_str() != "target") {
                 self.dirs.push(Box::new(p.clone()));
                 self.setnext(p.clone());
                 self.seekNext(*self.next.clone());
@@ -103,12 +102,25 @@ impl Crawler {
         }
     }
 
-    pub fn print(&self) -> () {
+    fn sort(&mut self) -> () {
+        self.dirs.sort();
+        self.files.sort();
+    }
+
+    fn sieve(&mut self) -> usize {
+        let initDirCnt: usize = self.dirs.len();
+        let initFileCnt: usize = self.files.len();
+        // dirCnt + fileCnt
+        usize::default()
+    }
+
+    pub fn print(&mut self) -> () {
+        self.sort();
         for d in self.dirs.iter() {
             println!("{}", d.display());
-            for f in self.files.iter() {
-                println!("----{}", f.display());
-            }
+            // for f in self.files.iter() {
+            //     println!("----{}", f.display());
+            // }
         }
     }
 }
